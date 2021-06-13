6d28885cf906cb1d18db73ebda352658a4ef5fe4
saransappa <saran.sappa@gmail.com>
2021-06-13 20:35:20 +0530

    added mailing functionality

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var multer = require('multer');      // For dealing with file uploads
 const fs = require('fs');     // For dealing with file system
+var nodemailer = require('nodemailer'); // For sending email
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -22,6 +23,7 @@ router.get('/', function(req, res, next) {
 /* Process java file */
 router.post('/submit',upload.single('file'),function(req,res,next){
 	console.log("------------file received---------");
+	console.log(req.body.email);
     if(req.file.originalname.endsWith(".java")){
 		//try {
 			// spawn a process to compile java file 
@@ -34,12 +36,37 @@ router.post('/submit',upload.single('file'),function(req,res,next){
 			var out = fs.openSync('./out.log', 'a');
 			var err = fs.openSync('./out.log', 'a');
 			var exec = require('child_process').exec;
+			res.send("Mail will be sent as soon as output is ready.");
 			var cmdString = 'cd uploads && java '+req.file.originalname;
 				exec(cmdString, (err, stdout, stderr) => {
 					console.log(err);
 					console.log(stderr);
 					console.log(stdout);
-					res.send(stdout);
+					//res.send("Mail will be sent as soon as output is ready.");
+					var transporter = nodemailer.createTransport({
+						service: 'gmail',
+						auth: {
+						  user: 'sss.events.notifications@gmail.com',
+						  pass: ''
+						}
+					  });
+				  
+					  var mailOptions = {
+						from: 'sss.events.notifications@gmail.com',
+						to: req.body.email,
+						subject: "JIVE output",
+						html : stdout
+					  };
+				  
+					  transporter.sendMail(mailOptions, function(error, info){
+						if (error) {
+						  console.log(error);
+						  return "failed";
+						} else {
+						  return info.response;
+						}
+					  }); 
+					  
 			});
 		/*} catch (error) {
 			console.log("----- Exited ------");
